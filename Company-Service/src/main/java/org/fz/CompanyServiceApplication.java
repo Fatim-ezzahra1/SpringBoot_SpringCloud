2e2bf46178c8265f523661a0796992625a7a3bb9
Fatim-Ezzahra <aitomgharfatimezzahra@gmail.com>
2024-02-28 20:53:38 +0100

    First Commit

---
+++ Company-Service/src/main/java/org/fz/CompanyServiceApplication.java
@@ -0,0 +1,42 @@
+package org.fz;
+
+import java.util.stream.Stream;
+
+import org.fz.dao.CompanyRepository;
+import org.fz.entities.Company;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CompanyServiceApplication /*implements CommandLineRunner*/ {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyServiceApplication.class, args);
+	}
+	
+	
+	
+	@Bean
+	CommandLineRunner start(CompanyRepository companyRepository) {
+		return args->{
+			Stream.of("A","B","C").forEach(cn->{
+				companyRepository.save(new Company(null,cn,100+Math.random()*900));
+			});
+			companyRepository.findAll().forEach(System.out::println);
+		};
+	}
+
+
+
+	/*@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println("Second Method");
+		// TODO Auto-generated method stub
+		
+	}*/
+
+}
+
